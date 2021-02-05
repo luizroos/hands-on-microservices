ae4defc6019480bcaa383d6a2eb25e582bcb2dc6
lroos <306955+luizroos@users.noreply.github.com>
2021-02-05 19:19:26 -0300

    docto

--- README.md
+++ README.md
@@ -1,52 +1,19 @@
-# Exercicio 7 - app com dependencia externa
+# Exercicio 8 - lidando com dependencias externa
 
-Rodando mock server
-
-```
-docker run -d --rm -p 1080:1080 --net=my-net --name mockserver mockserver/mockserver
-```
-
-* O parametro rm indica que o container pode ser excluido quando for desligado
-
-
-Vamos configurar o mock server para retornar ter uma resposta com delay de 1 segundo:
-
-```
-curl -v -X PUT "http://localhost:1080/expectation" -d '{
-  "httpRequest" : {
-    "method" : "GET",
-    "path" : "/postalcodes"
-  },
-  "httpResponse" : {
-    "body" : "{\"address\": \"rua mockada\", \"city\": \"Sao Paulo\", \"uf\": \"SP\"}",
-    "statusCode": 200,
-    "headers": [ { "name": "Content-Type", "values": ["application/json; charset=utf-8"] } ],
-    "delay": { "timeUnit": "MILLISECONDS", "value": 200 }
-  }}'
-```
-
-Veja mais opções: https://5-1.mock-server.com/mock_server/creating_expectations.html
-
-Teste nossa resposta mockada
-
-```
-curl http://localhost:1080/postalcodes
-```
-
-Veja agora as alterações em UserCreateService
+Execute a aplicação:
 
 ```
 docker network create my-net
 
-docker run -p 3306:3306 --name mysql --net=my-net -v ~/temp/mysql-data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=rootpass -e MYSQL_USER=db_user -e MYSQL_PASSWORD=db_pass -e MYSQL_DATABASE=sample-db -d mysql:5.6.51
+docker run --rm -p 3306:3306 --name mysql --net=my-net -v ~/temp/mysql-data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=rootpass -e MYSQL_USER=db_user -e MYSQL_PASSWORD=db_pass -e MYSQL_DATABASE=sample-db -d mysql:5.6.51
 
 ./gradlew build
 
-docker build --build-arg JAR_FILE=build/libs/*.jar -t user/sample-app:7 .
+docker build --build-arg JAR_FILE=build/libs/*.jar -t user/sample-app:8 .
 
-docker run --rm -p 8080:30001 -e MYSQL_HOST=mysql --name sample-app --net=my-net user/sample-app:7
+docker run --rm -p 8080:30001 -e MYSQL_HOST=mysql --name sample-app --net=my-net user/sample-app:8
 ```
 
-Voltamos o ID para UUID (caso tenha mantido a base do exercicio 6, execute um drop table na tabela do seu banco para a aplicação gerar a nova estrutura da tabela). Execute novamente o teste de carga com os parâmetros do exercicio 6.
+Rode novamente o teste de carga com os valores, compare os resultados com o teste de carga do exercicio 7.
 
-O que aconteceu? Reveja os parâmetros e descubra a partir de qual momento a aplicação começou a perder escalabilidade.
+O que mudou?
--- src/main/java/web/core/user/UserCreateService.java
+++ src/main/java/web/core/user/UserCreateService.java
@@ -2,12 +2,12 @@ package web.core.user;
 
 import java.util.Optional;
 
-import javax.transaction.Transactional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import web.core.exc.EntityAlreadyExistsException;
 import web.core.exc.UnknownPostalCodeException;
@@ -15,7 +15,7 @@ import web.core.postalcode.PostalCodeService;
 import web.core.postalcode.PostalCodeService.Address;
 
 @Service
-@Transactional(rollbackOn = Exception.class)
+@Transactional(propagation = Propagation.NEVER)
 public class UserCreateService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(UserCreateService.class);
@@ -26,14 +26,28 @@ public class UserCreateService {
 	@Autowired
 	private PostalCodeService postalCodeService;
 
-	public UserEntity createUser(String email, String name, int age, String addressPostalCode)
-			throws EntityAlreadyExistsException, UnknownPostalCodeException {
+	@Autowired
+	private UserCreateService userCreateService;
+
+	@Transactional(readOnly = true)
+	public void _validate(String email) throws EntityAlreadyExistsException {
 		LOGGER.info("Criando usuario {}", email);
 
 		final Optional<UserEntity> exist = userRepository.findUserByEmail(email);
 		if (exist.isPresent()) {
 			throw new EntityAlreadyExistsException(UserEntity.class, exist.get().getId(), "email", email);
 		}
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	public UserEntity _persist(String email, String name, int age, String addressPostalCode) {
+		LOGGER.info("Persistindo usuario {}", email);
+		return userRepository.createUser(email, name, age, addressPostalCode);
+	}
+
+	public UserEntity createUser(String email, String name, int age, String addressPostalCode)
+			throws EntityAlreadyExistsException, UnknownPostalCodeException {
+		userCreateService._validate(email);
 
 		LOGGER.info("Validando estado do cep {}", addressPostalCode);
 		final Address address = postalCodeService.getPostalCode(addressPostalCode);
@@ -41,9 +55,7 @@ public class UserCreateService {
 			throw new UnknownPostalCodeException(addressPostalCode);
 		}
 
-		LOGGER.info("Persistindo usuario {}", email);
-		final UserEntity user = userRepository.createUser(email, name, age, addressPostalCode);
-		return user;
+		return userCreateService._persist(email, name, age, addressPostalCode);
 	}
 
 }
