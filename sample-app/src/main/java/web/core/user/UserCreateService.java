2b77a148ab5fe27d64718be92a2423a482745978
lroos <306955+luizroos@users.noreply.github.com>
2021-02-17 17:26:24 -0300

    docto

--- sample-app/src/main/java/web/core/user/UserCreateService.java
+++ sample-app/src/main/java/web/core/user/UserCreateService.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Service;
 
 import web.core.exc.EntityAlreadyExistsException;
 import web.core.exc.UnknownPostalCodeException;
-import web.core.postalcode.PostalCodeService;
-import web.core.postalcode.PostalCodeService.Address;
 
 @Service
 public class UserCreateService {
@@ -20,9 +18,6 @@ public class UserCreateService {
 	@Autowired
 	private UserRepository userRepository;
 
-	@Autowired
-	private PostalCodeService postalCodeService;
-
 	public UserEntity createUser(String email, String name, int age, String addressPostalCode)
 			throws EntityAlreadyExistsException, UnknownPostalCodeException {
 		LOGGER.info("Criando usuario {}", email);
@@ -30,12 +25,6 @@ public class UserCreateService {
 		final Optional<UserEntity> exist = userRepository.findUserByEmail(email);
 		if (exist.isPresent()) {
 			throw new EntityAlreadyExistsException(UserEntity.class, exist.get().getId(), "email", email);
-		}
-
-		LOGGER.info("Validando estado do cep {}", addressPostalCode);
-		final Address address = postalCodeService.getPostalCode(addressPostalCode);
-		if (address == null || !address.getUf().equalsIgnoreCase("SP")) {
-			throw new UnknownPostalCodeException(addressPostalCode);
 		}
 
 		LOGGER.info("Persistindo usuario {}", email);
