664d04e979d52fddec22b2e72f3d51a5fcb68ed4
Luiz Gustavo Roos <luizroos@gmail.com>
2021-05-16 13:01:43 -0300

    Update bootstrap.sh

--- amazon/bootstrap.sh
+++ amazon/bootstrap.sh
@@ -2,7 +2,6 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker ${USER}
-newgrp docker
 
 ## instalação docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
